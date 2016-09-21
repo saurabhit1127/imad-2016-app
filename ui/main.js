12b4ffe358d7f64279d9567214407d5783ace75a
saurabhit1127 <saurabhit1127@gmail.com>
2016-09-21 21:33:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 //move the image
-var img=document.getElementbyId('madi');
+var img = document.getElementById('madi');
 img.onclick=function(){
-    img.style.marginleft='100px';
+    img.style.marginleft="100px";
 };
