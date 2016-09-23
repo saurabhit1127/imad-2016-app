43fc1904a2bbf45e8d4b27303452897e5f309478
saurabhit1127 <saurabhit1127@gmail.com>
2016-09-23 19:01:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ app.get('/', function (req, res) {
 
 
 
-app.get('/article one',function(req,res){
+app.get('/article-one',function(req,res){
     res.send('article one requested and served here');
 });
 
 app.get('/article two',function(req,res){
-    res.send('article two requested and served here');
+    res.send('article-two requested and served here');
 });
 
 
-app.get('/article three',function(req,res){
+app.get('/article-three',function(req,res){
     res.send('article three requested and served here');
 });
 
